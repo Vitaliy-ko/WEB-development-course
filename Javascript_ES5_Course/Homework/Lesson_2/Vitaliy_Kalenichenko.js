5baca3ff995ed5856a5c5fe8221f63e7fc1c1e51
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2019-07-10 23:30:15 +0300

    Add Homework_2

---
+++ Javascript_ES5_Course/Homework/Lesson_2/Vitaliy_Kalenichenko.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function getSquaresNumberInPoligon (n) {
+   let result = 1;
+
+   for (let i = 1;  i < n ; i++) {
+      result += i * 4;
+   }
+
+   return result;
+}
+
+console.log(getSquaresNumberInPoligon(2));
+
